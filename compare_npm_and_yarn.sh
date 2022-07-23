c58ba0e0c3c4ec0723dde1cc74193313e183cd8e
Mariia Mohylska <55410334+MariiaMohylska@users.noreply.github.com>
2022-07-23 15:02:37 -0400

    Add files via upload

---
+++ compare_npm_and_yarn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd example-app-nodejs-backend-react-frontend
+node install
+duration_start_npm=$(date +%s)
+npm install
+npm run build
+pm2 --name ExampleNodeJS start npm -- start 
+duration_end_npm=$(date +%s)
+sudo pkill -f node 
+npm cache clean
+rm package-lock.json
+
+node install
+duration_start_yarn=$(date +%s)
+yarn install
+yarn run build
+pm2 --name ExampleNodeJS start yarn -- start
+pkill -f node
+duration_end_yarn=$(date +%s)
+yarn cache clean
+rm package-lock.json
+
+pm2 delete all
+
+time_npm=$((duration_end_npm - duration_start_npm))
+time_yarn=$((duration_end_yarn  - duration_start_yarn))
+
+echo "NPM: $time_npm  | YARN: $time_yarn"
